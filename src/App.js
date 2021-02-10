edc862db17bac550a1d4c8b74baee0aecd0b8a03
VajihuddinAhmed <vajihahmed@gmail.com>
2021-02-10 03:09:12 +0100

    sign in page and related components added & header section page completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+        <Route exact path="/shop" component={ShopPage}/>
+        <Route exact path="/signin" component={SignInAndSignUpPage}/>
+      </Switch>
     </div>
   );
 }
