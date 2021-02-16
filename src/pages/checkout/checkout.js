7913ae45d9e6efc04f30dc71d8ddeb5ecd092cdd
VajihuddinAhmed <vajihahmed@gmail.com>
2021-02-16 22:03:37 +0100

    Added and updated stripe integration and updated our fav icon

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -4,6 +4,7 @@ import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item';
 import './checkout.scss';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button';
 
 const CheckoutPage = ({ cartItems, total }) => (
     <div className="checkout-page">
@@ -29,8 +30,16 @@ const CheckoutPage = ({ cartItems, total }) => (
         {cartItems.map((cartItem) => <CheckoutItem key={cartItem.id} cartItem={cartItem} /> )}
 
         <div className="total">
-            <span>TOTAL : €{total}</span>
+            <span>Total : €{total}</span>
         </div>
+
+        <div className="test-warning">
+            *Please use the following test credit card for payments*
+            <br/>
+            4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
+        </div>
+
+        <StripeCheckoutButton price={total} />
 
     </div>
 )
