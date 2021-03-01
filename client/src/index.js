eea1284d7e40e85082e9c369ad92c963ba32e278
VajihuddinAhmed <vajihahmed@gmail.com>
2021-03-01 19:49:16 +0100

    updating our code to be pwa compliant, added service worker, https, updated manifest

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { store, persistor} from './redux/store';
 import './index.css';
 import App from './App';
 import { PersistGate } from 'redux-persist/integration/react';
+import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -20,6 +21,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
