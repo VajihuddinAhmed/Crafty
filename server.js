7a6486274a1003992e3fe72053c587198c97e8be
VajihuddinAhmed <vajihahmed@gmail.com>
2021-03-01 20:54:31 +0100

    updating our code to be pwa compliant, added serviceWorker.js, https, updated manifest

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
@@ -11,12 +12,14 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(compression())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(enforce.HTTPS({ trustProtoHeader: true }))
+app.use(cors())
+
 
 if(process.env.NODE_ENV === 'production') {
+    app.use(compression())
+    app.use(enforce.HTTPS({ trustProtoHeader: true }))
     app.use(express.static(path.join(__dirname, 'client/build')))
 
     app.get('*', function(req, res) {
